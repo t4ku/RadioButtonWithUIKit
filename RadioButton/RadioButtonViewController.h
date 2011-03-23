b9a89a7db25fa973426bd127a2d08dfb1b36af4b
taku ohkawa <taku.okawa@gmail.com>
2011-03-23 10:44:05 +0900

    Initial Commit

---
+++ RadioButton/RadioButtonViewController.h
@@ -0,0 +1,15 @@
+//
+//  RadioButtonViewController.h
+//  RadioButton
+//
+//  Created by ohkawa on 11/03/23.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RadioButtonViewController : UIViewController {
+    
+}
+
+@end
